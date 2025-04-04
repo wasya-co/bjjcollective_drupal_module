ee61651282ab71eee7ed47584f9f24c91e7b8afb
mac_a2141 <victor@piousbox.com>
2025-04-03 22:26:56 -0500

    groking locations

--- src/Controller/LocationsController.php
+++ src/Controller/LocationsController.php
@@ -51,6 +51,44 @@ class LocationsController extends ControllerBase {
     ];
   }
 
+
+  /*
+   * /my/feed
+   *
+  **/
+  public function myFeed() {
+    $cuId = \Drupal::currentUser()->id();
+
+    // $user = \Drupal::currentUser();
+    // $user = User::load(\Drupal::currentUser()->id());
+
+    // test-1
+    $bid = 35;
+    $block = \Drupal\block_content\Entity\BlockContent::load($bid);
+    $render = \Drupal::entityTypeManager()->getViewBuilder('block_content')->view($block);
+
+    // @TODO: get locations that I subscribed to
+
+    $nodes = \Drupal::entityTypeManager()->getStorage('node');
+    $nodes_query  = \Drupal::entityQuery('node');
+
+    $my_locations = $nodes_query->condition('type', 'location');
+    $my_locations->condition('field_users', $cuId);
+
+    // logg($my_locations, 'my_locations');
+
+    return [
+      '#theme' => 'my_feed',
+      '#test_var_1' => $render,
+      // 'content' => [
+      // ],
+      // 'system_main' => $render,
+      // 'sidebar_first' => $render,
+      // '#markup' => "<h1>myFeed()</h1>",
+    ];
+  }
+
+
   /**
    * show()
   **/
