4d376338110a3cfe077c8b6adc3fc152855ecaa3
mac_a2141 <victor@piousbox.com>
2026-01-02 21:44:06 -0600

    autocommit

--- src/Controller/YoutubeChannelsController.php
+++ src/Controller/YoutubeChannelsController.php
@@ -21,15 +21,73 @@ class YoutubeChannelsController extends ControllerBase {
    * 2025-10-02 This is used!
   **/
   public function check(Request $request) {
-    $youtube_queue = \Drupal::queue('youtube_queue');
+    $config = \Drupal::config('ish_drupal_module.settings');
+    $api_key = $config->get('google_api_youtube_key');
+
     $nid = $request->attributes->get('node');
-    $youtube_queue->createItem([
-      'nid' => $nid,
-    ]);
+    $youtube_channel = Node::load($nid);
+
+    $tags_issue_ids = $youtube_channel->get('field_tags_issue')->getValue();
+    $tags_issue_ids = array_column($tags_issue_ids, 'target_id');
+    logg($tags_issue_ids, '$tags_issue_ids');
+
+    $channel_id     = $youtube_channel->get('field_channel_id')->value;
+    $n_videos = 10;
+    // &videoDuration=long
+    $url = 'https://www.googleapis.com/youtube/v3/search?key='.$api_key.'&channelId='.$channel_id.'&part=snippet,id&order=date&maxResults='.$n_videos.'&type=video';
+    logg($url, '$url');
+
+    $json = file_get_contents($url);
+    $decoded_json = json_decode($json, false);
+    logg($decoded_json, '$decoded_json');
+
+    $outs = [];
+
+    foreach($decoded_json->items as $item) {
+      $youtube_id = $item->id->videoId;
+      $youtube_title = YoutubeVideo::title($youtube_id);
+
+      // $issue_uuid = '35'; // '4ac9695b-0854-4972-8528-1f52e21d2235'; // taxonomy_term/35 :: 2024q1-issue
+      $node_manager  = \Drupal::entityTypeManager()->getStorage('node');
+      $existing_page_youtube = $node_manager->loadByProperties([
+        'type' => 'page_youtube',
+        'field_youtube_id' => $youtube_id,
+      ]);
+      if (!$existing_page_youtube) {
+        $outs[ $item->id->videoId ] = $youtube_title;
+
+        $body = <<<AOL
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class='embed-responsive-item' src="https://www.youtube.com/embed/$youtube_id" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+              encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen></iframe>
+          </div>
+        AOL;
+
+        $new_item = $node_manager->create([
+          'uid' => $youtube_channel->getOwnerId(),
+          'body' => [
+            'value' => $body,
+            'format' => 'full_html',
+          ],
+          'field_youtube_id' => $youtube_id,
+          // 'field_issue' => [ 'target_id' => $issue_uuid ],
+          'field_tags_issue' => $tags_issue_ids,
+          'status' => 1, // is published
+          'title' => $youtube_title,
+          'type' => 'page_youtube',
+        ]);
+        $new_item->save();
+        \Drupal::messenger()->addMessage('Item From Youtube has been saved.');
+      }
+    }
+
+    logg($outs, '$outs');
 
     return [
       '#theme' => 'youtube_channels_check',
-      // '#decoded_json' => $json,
+      '#decoded_json' => $json,
+      '#outs' => $outs,
       '#abba' => 'given abba',
     ];
   }
