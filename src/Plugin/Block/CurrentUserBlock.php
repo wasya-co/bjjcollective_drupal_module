87f83bc6c622d49ed5566a1543493a42c4bfd945
mac_a2141 <victor@piousbox.com>
2026-01-03 13:31:12 -0600

    autocommit

--- src/Plugin/Block/CurrentUserBlock.php
+++ src/Plugin/Block/CurrentUserBlock.php
@@ -6,25 +6,27 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\Attribute\Block;
 
 /**
-* @Block(
-*   id = "current_user_block",
-*   admin_label = "current_user_block",
-* )
-*/
+ * @Block(
+ *   id = "current_user_block",
+ *   admin_label = "current_user_block",
+ * )
+**/
 class CurrentUserBlock extends BlockBase {
 
   /**
    * {@inheritdoc}
-   */
+  **/
   public function build() {
     $uid = \Drupal::currentUser()->id();
     $user = \Drupal\user\Entity\User::load($uid);
-    $email = $user->getEmail();
+    $current_user_email = $user->getEmail();
 
     return [
-      // '#markup' => $email,
       '#theme' => 'current_user_block',
-      '#email' => $email,
+      '#current_user_email' => $current_user_email,
+      '#cache' => [
+        'contexts' => ['user'],
+      ],
     ];
   }
 
